de657fdcc2ab879d25988ef5d1361966bce9011d
Vadim <modstils88@mail.ru>
2024-10-07 15:34:51 +0300

    added cors and res.json(rows

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,23 @@
 import express, { Request, Response } from "express";
 import pool from "./db.js";
 const app = express();
-
+import cors from "cors";
+app.use(cors());
 app.get("/welcome/:city", (req: Request, res: Response) => {
-    const { city} = req.params;
-   if (!city) {
+  const { city } = req.params;
+  if (!city) {
     return res.status(400).send("Необходимо указать город");
-   }
-   const greetingMessage = ` Привет всем из ${city}!`;
-   res.send({message: greetingMessage});
-})
-
+  }
+  const greetingMessage = ` Привет всем из ${city}!`;
+  res.send({ message: greetingMessage });
+});
 app.get("/city/list", async (req: Request, res: Response) => {
-    try {
-        const { rows} = await pool.query('SELECT city_name From cities');
-    const cities  = rows.map( row => row.city_name);
-
-res.send(cities);
-    } catch( err) {
-        console.error(err);
-        res.status(500).send("Произошла ошибка");
-    }
-})
-export default app
+  try {
+    const { rows } = await pool.query("SELECT id, city_name FROM cities");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Произошла ошибка");
+  }
+});
+export default app;

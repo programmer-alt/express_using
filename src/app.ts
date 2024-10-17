01a3df2135a2b43cfde7ab381a802f64c7df5695
Vadim <modstils88@mail.ru>
2024-10-17 17:53:00 +0300

    added newCity in the app.post

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import pool from "./db.js";
 const app = express();
 import cors from "cors";
 app.use(cors());
+app.use(express.json());
 app.get("/welcome/:city", (req: Request, res: Response) => {
   const { city } = req.params;
   if (!city) {
@@ -33,3 +34,17 @@ app.delete('/cities/:id', async (req: Request, res: Response) => {
         res.status(500).send("Произошла ошибка");
     }
 })
+
+app.post('/cities/add', async (req: Request, res: Response) => {
+  console.log('получен запрос на добавление города');
+  const { city_name } = req.body;
+  
+  try {
+    const addCity= await pool.query('INSERT INTO cities (city_name) Values ($1) RETURNING id', [city_name]);
+    const newCity = addCity.rows[0]
+    res.json(newCity);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Произошла ошибка");
+  }
+});

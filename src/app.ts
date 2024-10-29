1ed327a5cd38988a059170b6895eb00e16a1c86f
Vadim <modstils88@mail.ru>
2024-10-29 19:54:32 +0300

    delete postgres future

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import pool from "./db.js";
 const app = express();
 import cors from "cors";
 app.use(cors());
- postgres-feature
+
 app.use(express.json());
 app.get("/welcome/:city", (req: Request, res: Response) => {
   const { city } = req.params;

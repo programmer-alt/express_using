2337b785bfab529fe1b8aee076490a9bdffbeedd
Vadim <modstils88@mail.ru>
2024-11-08 18:35:13 +0300

    Add changes

--- build/main.js
+++ build/main.js
@@ -1,13 +1,10 @@
 import App from "./app.js";
 import * as http from 'http';
-
 const port = 3000;
-const server = http.createServer(App); 
-
-server.listen(port, () => { 
+const server = http.createServer(App);
+server.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
 });
-
 process.on('SIGINT', () => {
     console.log('Получен сигнал остановки');
     server.close(() => {
--- src/db.ts
+++ src/db.ts
@@ -2,5 +2,22 @@ import pg from "pg";
 import configPostgresConnect from "./configPostgresConnect.js";
 const { Pool } = pg;
 
+
 const pool = new Pool(configPostgresConnect.db);
+const connectWithRetry = async () => {
+    let retries = 5;
+    while (retries) {
+try {
+    await pool.connect()
+    console.log(' База данных успешно подключена')
+ break;
+} catch (err) {
+    retries -= 1
+    console.log(`Не удалось подключиться к базе данных, осталось попыток: ${retries}`)
+    await new Promise(resolve => setTimeout(resolve, 5000))
+}
+    }
+    
+}
+connectWithRetry();
 export default pool;

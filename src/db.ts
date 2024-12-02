0ce507a376b83e5e2697ff09962db4a715d8e038
Vadim <modstils88@mail.ru>
2024-12-02 17:19:23 +0300

    added singlton in the db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,22 +2,34 @@ import pg from "pg";
 import configPostgresConnect from "./configPostgresConnect.js";
 const { Pool } = pg;
 
+class DatabaseCities {
+    private static instance: DatabaseCities;
+      pool: InstanceType<typeof Pool>;
 
-const pool = new Pool(configPostgresConnect.db);
-const connectWithRetry = async () => {
+constructor() {
+ this.pool = new Pool(configPostgresConnect.db)
+}
+public static getInstance(): DatabaseCities {
+ if (!DatabaseCities.instance) {
+    DatabaseCities.instance = new DatabaseCities();
+}
+return DatabaseCities.instance;
+}
+ public async connectWithRetry():Promise<void> {
     let retries = 5;
-    while (retries) {
-try {
-    await pool.connect()
-    console.log(' База данных успешно подключена')
- break;
-} catch (err) {
-    retries -= 1
-    console.log(`Не удалось подключиться к базе данных, осталось попыток: ${retries}`)
-    await new Promise(resolve => setTimeout(resolve, 5000))
+    while(retries) {
+        try {
+            await this.pool.connect();
+            console.log(' База данных успешно подключена')
+            break;
+        } catch (err) { 
+            retries -= 1
+            console.log(`Ошибка подключения к базе данных. Попытка ${retries}`)
+            await new Promise(resolve => setTimeout(resolve, 5000))
+        }
+ }
+ throw new Error(' Не удалось подключиться к базе данных после 5 попыток')
 }
-    }
-    
 }
-connectWithRetry();
-export default pool;
+
+export default DatabaseCities.getInstance();

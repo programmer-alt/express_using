cb611e526f7492be31af91aa793393b974dd99b4
Vadim <modstils88@mail.ru>
2024-12-02 17:44:39 +0300

    changed import app.js

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import App from "./app.js";
+import App from './app.js';
 import * as http from 'http';
-
 const port = 3000;
-const server = http.createServer(App); 
+const server = http.createServer(App);
 
-server.listen(port, () => { 
+server.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
 });
-
 process.on('SIGINT', () => {
     console.log('Получен сигнал остановки');
     server.close(() => {
